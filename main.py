8f8d05072d62d8f9df1ed3ff8efbc250ded47dd6
ilansanmartin <ilansanmartink@uc.cl>
2025-09-06 12:25:01 -0400

    base solicitudes

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from routers import solicitudes
 
 app = FastAPI()
-@app.get("/inicio")
+
+app.include_router(solicitudes.router)
+
+@app.get("/")
 def correr_back():
     return {"mensaje": "Hola, mundo!"}
---
+++ routers/solicitudes.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List, Optional
+
+router = APIRouter()
+
+# Datos solicitud de los pacientes
+class Solicitud(BaseModel):
+    id: int
+    cama_id: int
+    area: str # Aseo, alimentación, mantención, etc
+    descripcion: str
+    fecha_solicitud: datetime
+    estado: str = "pendiente"  # pendiente, en_proceso, completada
+    fecha_completacion: Optional[datetime] = None
+
+# Base de datos
+solicitudes_db = []
+
+# Crear una nueva solicitud
+@router.post("/solicitudes")
+def crear_solicitud(solicitud: Solicitud):
+    solicitudes_db.append(solicitud)
+    return {"mensaje": "Solicitud creada", "solicitud": solicitud}
+
+# Obtener todas las solicitudes
+@router.get("/solicitudes")
+def obtener_solicitudes():
+    return solicitudes_db
